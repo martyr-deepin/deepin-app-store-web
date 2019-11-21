b4d9330cab367e3d10198ef33dd04f8a37737549
myml <wurongjie1@gmail.com>
2019-11-21 11:12:13 +0800

    fix: score

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'environments/environment';
 import { map, shareReplay } from 'rxjs/operators';
 
-import { APIBase } from './api';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root',
