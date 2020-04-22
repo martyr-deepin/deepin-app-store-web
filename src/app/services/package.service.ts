b10b039f658e8917e5c3e2061e877f80d3bca8b4
myml <wurongjie1@gmail.com>
2020-04-22 19:50:14 +0800

    feat: pacakge query cache

--- src/app/services/package.service.ts
+++ src/app/services/package.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
-import { StoreService } from 'app/modules/client/services/store.service';
-import { Subject } from 'rxjs';
+import { StoreService, Package } from 'app/modules/client/services/store.service';
+import { Subject, of } from 'rxjs';
 import { filter, share, map, buffer, switchMap, first, debounceTime } from 'rxjs/operators';
-import { BlacklistService } from './blacklist.service';
+import { JobService } from './job.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PackageService {
-  constructor(private storeService: StoreService, private blacklistService: BlacklistService) {}
-  backlist$ = this.blacklistService.blacklist();
-  private q$ = new Subject<QueryOption>();
-  private r$ = this.q$.pipe(
-    buffer(this.q$.pipe(debounceTime(100))),
-    switchMap(opts => {
-      return this.storeService.query(opts);
-    }),
-    share(),
-  );
+  constructor(private storeService: StoreService, private jobService: JobService) {
+    this.start();
+    this.jobService.jobList().subscribe(() => this.cache.clear());
+  }
+  private result$ = new Subject<Map<string, Package>>();
+  private cache = new Map<string, Package>();
+  private queryArr: QueryOption[] = [];
+
+  async start() {
+    while (true) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      if (this.queryArr.length === 0) {
+        continue;
+      }
+      const arr = this.queryArr.splice(0, 20);
+      const m = await this.storeService.query(arr).toPromise();
+      m.forEach((pkg, name) => this.cache.set(name, pkg));
+      this.result$.next(m);
+    }
+  }
+
   query(opt: QueryOption) {
-    setTimeout(() => this.q$.next(opt));
-    return this.r$.pipe(
+    if (this.cache.has(opt.name)) {
+      return of(this.cache.get(opt.name));
+    }
+    setTimeout(() => this.queryArr.push(opt));
+    return this.result$.pipe(
       filter(m => m.has(opt.name)),
       map(m => m.get(opt.name)),
       first(),
@@ -28,7 +42,16 @@ export class PackageService {
   }
 
   async querys(opts: QueryOption[]) {
-    return this.storeService.query(opts).toPromise();
+    let result = new Map<string, Package>();
+    opts.forEach(opt => this.cache.has(opt.name) && result.set(opt.name, this.cache.get(opt.name)));
+    if (result.size < opts.length) {
+      const mm = await this.storeService.query(opts.filter(opt => !result.has(opt.name))).toPromise();
+      mm.forEach((pkg, name) => {
+        this.cache.set(name, pkg);
+        result.set(name, pkg);
+      });
+    }
+    return result;
   }
 }
 
