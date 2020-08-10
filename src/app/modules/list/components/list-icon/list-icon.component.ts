5ac49fa0d233d232963fee25de274b6e35d5e941
王铎 <panel.wang@musjoy.com>
2020-08-10 15:55:48 +0800

    feat: application list arrangement flexible center, auto wraps.
    1.change the list display 'inline-block' to 'grid-item'.
    2.on window resize, the item auto wrap.
    
    Log: application list arrangement flexible center, auto wraps.

--- src/app/modules/list/components/list-icon/list-icon.component.ts
+++ src/app/modules/list/components/list-icon/list-icon.component.ts
@@ -11,17 +11,18 @@ import {
   ViewEncapsulation,
   OnDestroy,
 } from '@angular/core';
-import { WaitIconComponent } from 'app/modules/share/components/wait-icon/wait-icon.component';
+import { environment } from 'environments/environment';
+import { Subscription, fromEvent, BehaviorSubject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'dstore-list-icon',
   templateUrl: './list-icon.component.html',
   styleUrls: ['./list-icon.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ListIconComponent implements OnInit, OnChanges,OnDestroy {
-  constructor() {}
-  @ViewChild('loadingRef', { static: true }) elRef: ElementRef<HTMLDivElement>;
-  @ViewChild('waitIcon', { static: true }) waitIcon: WaitIconComponent;
+export class ListIconComponent implements OnInit, OnChanges, OnDestroy {
+  constructor(private el: ElementRef<HTMLDivElement>) {}
+  @ViewChild('loadingRef', { static: true }) loadingRef: ElementRef<HTMLDivElement>;
   @Input() list: [];
   @Input() lazyload = false;
   @Input() nameColor: string;
@@ -29,20 +30,33 @@ export class ListIconComponent implements OnInit, OnChanges,OnDestroy {
   @Input() slogan = false;
   @Output() load = new EventEmitter<void>();
   wait = false;
+  reloadColumn$ = new BehaviorSubject<boolean>(false);
 
   // 监听是否到达底部
   intersection = new IntersectionObserver(([e]: IntersectionObserverEntry[]) => {
     if (e.isIntersecting) {
       this.wait = true;
       this.load.next();
-      this.intersection.unobserve(this.elRef.nativeElement);
+      this.intersection.unobserve(this.loadingRef.nativeElement);
     }
   });
 
-  ngOnInit(): void {}
-  
+  onResizeSubscription = fromEvent(window, 'resize').subscribe((event) => {
+    this.reloadColumn$.next(true);
+  });
+
+  reloadSubscription = this.reloadColumn$.pipe(debounceTime(100)).subscribe(() => {
+    this.gridGroup();
+  });
+
+  ngOnInit(): void {
+    //this.gridGroup();
+  }
+
   ngOnDestroy(): void {
-    this.intersection.disconnect()
+    this.intersection.disconnect();
+    this.onResizeSubscription.unsubscribe();
+    this.reloadSubscription.unsubscribe();
   }
 
   ngOnChanges(changed: SimpleChanges) {
@@ -63,10 +77,21 @@ export class ListIconComponent implements OnInit, OnChanges,OnDestroy {
       if (this.lazyload) {
         console.log('loading');
         var i = setTimeout(() => {
-          this.intersection.observe(this.elRef.nativeElement)
-          clearTimeout(i)
+          this.intersection.observe(this.loadingRef.nativeElement);
+          clearTimeout(i);
         }, 500);
       }
     }
+  }
+
+  //auto grid col
+  gridGroup() {
+    const itemIcon = <HTMLDivElement>this.el.nativeElement.querySelector('.item-icon');
+    const colNum = Math.floor(itemIcon.clientWidth / (environment.fontSize * 19));
+    let itemWidth = '';
+    for (let i = 0; i < colNum; i++) {
+      itemWidth += ' 1fr';
+    }
+    itemIcon.style.gridTemplateColumns = itemWidth;
   }
 }
