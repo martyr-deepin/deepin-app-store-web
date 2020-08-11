d4b74210bb1f08a9fa596a39db6379d124553383
jianghao <jianghao@uniontech.com>
2020-08-11 15:37:33 +0800

    fix: add myUpdate module private auth
    app update function add private auth logic
    
    Log: finish private auth logic

--- src/app/modules/my-updates/components/list-item/list-item.component.ts
+++ src/app/modules/my-updates/components/list-item/list-item.component.ts
@@ -1,76 +1,97 @@
-import { Component, Input, ElementRef } from '@angular/core';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 import { Software, SoftwareService } from 'app/services/software.service';
-import { StoreJobInfo, StoreJobStatus, StoreJobError, StoreJobErrorType, CanFixError } from 'app/modules/client/models/store-job-info';
+import {
+  StoreJobInfo,
+  StoreJobStatus,
+  StoreJobError,
+  StoreJobErrorType,
+  CanFixError,
+} from 'app/modules/client/models/store-job-info';
 import { switchMap, filter } from 'rxjs/operators';
 import { StoreService } from 'app/modules/client/services/store.service';
 import { JobService } from 'app/services/job.service';
 import { MyUpdatesService } from '../../services/my-updates.service';
 import { Subscription } from 'rxjs';
 import { SysAuthService } from 'app/services/sys-auth.service';
+import { environment } from 'environments/environment';
+import { StoreMode } from 'app/services/storeMode';
 
 @Component({
   selector: 'my-updates-list-item',
   templateUrl: './list-item.component.html',
-  styleUrls: ['./list-item.component.scss']
+  styleUrls: ['./list-item.component.scss'],
 })
-export class ListItemComponent {
-
+export class ListItemComponent implements OnInit {
   constructor(
-    private service:MyUpdatesService,
+    private service: MyUpdatesService,
     private softwareService: SoftwareService,
     private jobService: JobService,
     private storeService: StoreService,
     private sysAuth: SysAuthService,
-    private el: ElementRef
+    private el: ElementRef,
   ) {}
+  ngOnInit(): void {
+    if (environment.appStoreType === StoreMode.IntranetAppStore) {
+      this.sysAuth.noIntranetAuth$.subscribe((v) => {
+        this.privateStoreAuth = v;
+      });
+    }
+  }
 
-  @Input() software:Software;
+  @Input() software: Software;
 
-  @Input() job:StoreJobInfo=null;
-  
+  @Input() job: StoreJobInfo = null;
+
   StoreJobStatus = StoreJobStatus;
 
-  subscribe:Subscription;
-  inited:number = 0;
+  subscribe: Subscription;
+  inited: number = 0;
 
- 
+  privateStoreAuth = false;
 
   update(): void {
-    if(!this.service.updatings.get(this.software.package_name)){
-      this.service.updatings.set(this.software.package_name,this.software)
+    /**
+     * private store auth logic
+     */
+    if (environment.appStoreType === StoreMode.IntranetAppStore && !this.privateStoreAuth) {
+      this.sysAuth.setAuthMessage();
+      return;
     }
-    this.softwareService.install(this.software)
+    if (!this.service.updatings.get(this.software.package_name)) {
+      this.service.updatings.set(this.software.package_name, this.software);
+    }
+    this.softwareService.install(this.software);
   }
 
   //忽略当前版本
-  ignoreVersion(){
-    let apps = this.service.getIgnoreApps()
-    if(!apps) {
-      apps = {}
+  ignoreVersion() {
+    let apps = this.service.getIgnoreApps();
+    if (!apps) {
+      apps = {};
     }
-    apps[this.software.id]=this.software.package.remoteVersion
-    this.service.setIgnoreApp(apps)
+    apps[this.software.id] = this.software.package.remoteVersion;
+    this.service.setIgnoreApp(apps);
   }
 
   //开始任务
-  start = (id:string) => {
+  start = (id: string) => {
     this.jobService.startJob(id);
-  }
+  };
 
   //暂停任务
-  pause = (id:string) => this.jobService.stopJob(id);
+  pause = (id: string) => this.jobService.stopJob(id);
 
   //取消任务
-  cancelled:boolean = false;
-  cancel = (job:string) => {
-    this.service.updatings.delete(this.software.package_name)
+  cancelled: boolean = false;
+  cancel = (job: string) => {
+    this.service.updatings.delete(this.software.package_name);
     this.cancelled = true;
-    this.jobService.clearJob(job)
+    this.jobService.clearJob(job);
   };
 
   //任务失败重试
   fixing = false;
-  retry(job:StoreJobInfo) {
+  retry(job: StoreJobInfo) {
     let err: StoreJobError;
     try {
       err = JSON.parse(job.description) as StoreJobError;
@@ -81,38 +102,37 @@ export class ListItemComponent {
     if (CanFixError.includes(err.ErrType)) {
       this.fixing = true;
       this.storeService
-      .fixError(err.ErrType.toString().split('::')[1])
-      .pipe(
-        switchMap(
-          () => this.storeService.jobListChange(),
-          (jobPath, jobList) => jobList.includes(jobPath),
-        ),
-        filter(exists => !exists),
-      )
-      .subscribe(() => {
-        this.fixing = false;
-        this.storeService.resumeJob(job.job);
-      });
+        .fixError(err.ErrType.toString().split('::')[1])
+        .pipe(
+          switchMap(
+            () => this.storeService.jobListChange(),
+            (jobPath, jobList) => jobList.includes(jobPath),
+          ),
+          filter((exists) => !exists),
+        )
+        .subscribe(() => {
+          this.fixing = false;
+          this.storeService.resumeJob(job.job);
+        });
     }
   }
 
   //判断是否溢出
-  judgeOverflow$ = new Promise((resove)=>{
-    var timeout = setTimeout(()=>{
-      clearTimeout(timeout)
-      resove(this.judge_overflow())
-    })
-  })
+  judgeOverflow$ = new Promise((resove) => {
+    var timeout = setTimeout(() => {
+      clearTimeout(timeout);
+      resove(this.judge_overflow());
+    });
+  });
 
   judge_overflow() {
-    const nativeElement = this.el.nativeElement
-    const log_content = nativeElement.querySelector('.log_content')
+    const nativeElement = this.el.nativeElement;
+    const log_content = nativeElement.querySelector('.log_content');
     return log_content.scrollHeight > log_content.clientHeight || false;
   }
 
   sysAuthStatus$ = this.sysAuth.sysAuthStatus$;
   sysAuthMessage() {
-    this.sysAuth.authorizationNotify()
+    this.sysAuth.authorizationNotify();
   }
-
 }
--- src/app/modules/my-updates/my-updates.component.ts
+++ src/app/modules/my-updates/my-updates.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MyUpdatesService } from './services/my-updates.service';
 import { SoftwareService } from 'app/services/software.service';
 import { first, switchMap } from 'rxjs/operators';
@@ -6,130 +6,142 @@ import { JobService } from 'app/services/job.service';
 import { StoreJobType, StoreJobStatus } from '../client/models/store-job-info';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { SysAuthService } from 'app/services/sys-auth.service';
+import { environment } from 'environments/environment';
+import { StoreMode } from 'app/services/storeMode';
 
 @Component({
-  selector:"dstore-my-updates",
+  selector: 'dstore-my-updates',
   templateUrl: './my-updates.component.html',
   styleUrls: ['./my-updates.component.scss'],
 })
-export class MyUpdatesComponent implements OnInit,OnDestroy{
-  
+export class MyUpdatesComponent implements OnInit, OnDestroy {
   constructor(
-    public service:MyUpdatesService,
-    private softwareService:SoftwareService,
-    private jobService:JobService,
-    private router:Router,
-    private route:ActivatedRoute,
-    private sysAuth: SysAuthService
-  ){}
+    public service: MyUpdatesService,
+    private softwareService: SoftwareService,
+    private jobService: JobService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private sysAuth: SysAuthService,
+  ) {}
 
   sysAuthStatus$ = this.sysAuth.sysAuthStatus$;
-
-  ngOnInit(){
-    this.path = this.route.firstChild.snapshot.routeConfig['path']
+  //private store auth
+  privateStoreAuth = true;
+  ngOnInit() {
+    if (environment.appStoreType === StoreMode.IntranetAppStore) {
+      this.sysAuth.noIntranetAuth$.subscribe((v) => {
+        this.privateStoreAuth = v;
+      });
+    }
+    this.path = this.route.firstChild.snapshot.routeConfig['path'];
     // init subscribe
-    this.selectChange(0)
+    this.selectChange(0);
   }
 
-  dueTime:number=1000*60*60*24*30;
-  selectChange(value:number) {
-    let nowTime = new Date().setHours(0,0,0,0)+1000*60*60*24
-    if(value){
+  dueTime: number = 1000 * 60 * 60 * 24 * 30;
+  selectChange(value: number) {
+    let nowTime = new Date().setHours(0, 0, 0, 0) + 1000 * 60 * 60 * 24;
+    if (value) {
       this.dueTime = value;
     }
     // Query updated list
-    let apps = this.service.getRecentlyApps()
-    if(apps) {
-      const beforeTime = nowTime - this.dueTime
-      for(var key in apps) {
-        if((Number(apps[key].updated_at)-0) < beforeTime) {
-          delete apps[key]
+    let apps = this.service.getRecentlyApps();
+    if (apps) {
+      const beforeTime = nowTime - this.dueTime;
+      for (var key in apps) {
+        if (Number(apps[key].updated_at) - 0 < beforeTime) {
+          delete apps[key];
         }
       }
-      this.service.recentlyApps$.next(apps)
+      this.service.recentlyApps$.next(apps);
     }
-
   }
 
-  jobList = []
+  jobList = [];
 
   // Runing jobs status
   jobsStatus$ = this.jobService.jobsInfo().pipe(
-    switchMap(async infoList => {
+    switchMap(async (infoList) => {
       const CountType = [StoreJobType.install, StoreJobType.download];
       // Query renewableApps
-      const apps = await this.service.renewableApps$.pipe(first()).toPromise()
-      const packageNames = apps.map(app=>app.package_name)
-      const result = infoList.filter(info => CountType.includes(info.type))
-        .filter(info => packageNames.includes(info.packages[0]) );
+      const apps = await this.service.renewableApps$.pipe(first()).toPromise();
+      const packageNames = apps.map((app) => app.package_name);
+      const result = infoList
+        .filter((info) => CountType.includes(info.type))
+        .filter((info) => packageNames.includes(info.packages[0]));
       let status = 0;
-      if(result.length>0) {
+      if (result.length > 0) {
         status = 1;
-        if(result.filter(info => info.status === StoreJobStatus.paused).length === result.length) {
+        if (result.filter((info) => info.status === StoreJobStatus.paused).length === result.length) {
           status = 2;
         }
       }
       this.jobList = result;
       return status;
-    })
-  )
+    }),
+  );
 
   // Recent updated count
   recentlyLength$ = this.service.recentlyApps$.pipe(
-    switchMap(async result => {
-      const recents = Object.keys(result).map(val => ({id:parseInt(val),updateDate:result[val]}))
+    switchMap(async (result) => {
+      const recents = Object.keys(result).map((val) => ({ id: parseInt(val), updateDate: result[val] }));
       return recents.length;
-    })
-  )
+    }),
+  );
 
   /**
    * Update all
    */
-  updateAll(){
+  updateAll() {
+    /**
+     * private store auth logic
+     */
+    if (environment.appStoreType === StoreMode.IntranetAppStore && !this.privateStoreAuth) {
+      this.sysAuth.setAuthMessage();
+      return;
+    }
     const res = this.service.softCache;
-    if(res.length) {
-      res.map(soft => {
-        this.service.updatings.set(soft.package_name,soft)
-      })
-      this.softwareService.install(...res)
+    if (res.length) {
+      res.map((soft) => {
+        this.service.updatings.set(soft.package_name, soft);
+      });
+      this.softwareService.install(...res);
     }
   }
 
   /**
    * Pause All
    */
-  pauseAll(){
-    this.jobList.forEach(e => {
-      this.jobService.stopJob(e.job)
-    })
+  pauseAll() {
+    this.jobList.forEach((e) => {
+      this.jobService.stopJob(e.job);
+    });
   }
 
   /**
    * Start all
    */
   startAll() {
-    this.jobList.forEach(e => {
-      this.jobService.startJob(e.job)
-    })
+    this.jobList.forEach((e) => {
+      this.jobService.startJob(e.job);
+    });
   }
 
   // Switch router path
-  path:string;
-  path$ = this.router.events.subscribe(event=>{
-    if(event instanceof NavigationEnd) {
-      this.path = this.route.firstChild.snapshot.routeConfig['path']
+  path: string;
+  path$ = this.router.events.subscribe((event) => {
+    if (event instanceof NavigationEnd) {
+      this.path = this.route.firstChild.snapshot.routeConfig['path'];
     }
-  })
+  });
 
-
-  ngOnDestroy(){
-    if(this.path$) {
-      this.path$.unsubscribe()
+  ngOnDestroy() {
+    if (this.path$) {
+      this.path$.unsubscribe();
     }
   }
 
   sysAuthMessage() {
-    this.sysAuth.authorizationNotify()
+    this.sysAuth.authorizationNotify();
   }
-
 }
